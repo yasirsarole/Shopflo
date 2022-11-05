4ada75f10bd296a53b8d0037b33590dc583fb901
Yasir Sarole <yasirsarole81@gmail.com>
2022-11-05 21:06:58 +0530

    Added filter by text

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const count = parseInt(rangeCounter.textContent);
 
     if (title && subTitle && count < 5) {
-      containerTiles.innerHTML += `<div class="tile" style="background-color:${color};"><span class="title">${title}</span><span class="subtitle">${subTitle}</span></div>`;
+      containerTiles.innerHTML += `<div class="tile" data-content="${title + ' ' + subTitle}" style="background-color:${color};"><span class="title">${title}</span><span class="subtitle">${subTitle}</span></div>`;
       rangeCounter.textContent = count + 1;
       document.querySelector(".range-container__range").style.background = `linear-gradient(to right, #000 ${(count + 1) * 20}%,#ffffff ${(count + 1) * 20}%)`
     } else {
@@ -81,5 +81,54 @@ document.addEventListener("DOMContentLoaded", () => {
       e.target.classList.add('selected');
       document.querySelector('.hidden-color').value = e.target.style['background-color'];
     }
-  })
+  });
+
+  // filter by color
+  const containerFilterList = document.querySelector('.container__filter__list');
+  const tiles = document.querySelectorAll('.tile');
+  containerFilterList.addEventListener('click', (e) => {
+    if (e.target.nodeName === 'LI') {
+      const color = e.target.style['background-color'];
+
+      if (e.target.classList.contains('selected')) {
+        e.target.classList.remove('selected');
+
+        tiles.forEach(tile => {
+          tile.classList.remove('hide');
+        })
+      } else {
+        e.target.classList.add('selected');
+
+        tiles.forEach(tile => {
+          if (tile.style['background-color'] !== color) {
+            tile.classList.add('hide');
+          }
+        })
+      }
+    }
+  });
+
+  // filter by text
+  const debounce = (fn, delay) => {
+    let timer;
+    return (...args) => {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        fn(...args);
+      }, delay);
+    };
+  };
+  const filteredText = document.querySelector('.container__filter__search');
+  filteredText.addEventListener('input', debounce((e) => {
+    const value = e.target.value;
+    document.querySelectorAll('.tile').forEach(tile => {
+      tile.classList.remove('hide');
+
+      if (!tile.dataset.content.includes(value)) {
+        tile.classList.add('hide');
+      } else {
+        tile.classList.remove('hide');
+      }
+    })
+  }, 1000))
 })
